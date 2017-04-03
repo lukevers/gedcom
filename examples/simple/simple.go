afce27ec90a02f48588f82ff065e5e813faaa467
Luke Evers <luke@lukevers.com>
2017-04-03 10:00:38 -0400

    Only output information if it exists in the family

--- examples/simple/simple.go
+++ examples/simple/simple.go
@@ -18,13 +18,17 @@ func main() {
 
 	for i, family := range tree.Families {
 		log.Printf("===== FAMILY %d =====", i)
-		name, _ := family.Father.GetAttribute("NAME")
-		log.Printf("Father: %s", name)
-		name, _ = family.Mother.GetAttribute("NAME")
-		log.Printf("Mother: %s", name)
+		if family.Father != nil {
+			name, _ := family.Father.GetAttribute("NAME")
+			log.Printf("Father: %s", name)
+		}
+		if family.Mother != nil {
+			name, _ := family.Mother.GetAttribute("NAME")
+			log.Printf("Mother: %s", name)
+		}
 
 		for j, child := range family.Children {
-			name, _ = child.GetAttribute("NAME")
+			name, _ := child.GetAttribute("NAME")
 			log.Printf("Child %d: %s", j, name)
 		}
 	}
